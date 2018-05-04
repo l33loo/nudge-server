98fcbcc02cb6fd11aa1a5e0d8af14ff3272085a5
Lila Karpowicz <karpowicz.lila@gmail.com>
2018-05-04 03:46:31 +0000

    Add get home route

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const path = require('path');
 const PORT = 3000;
+const app = express()
 
-express()
+app
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', jsx)
@@ -15,6 +16,8 @@ const clients = ['carl', (Date.now() - 60000)];
 // When a client connects they are assigned a socket, represented by
 // the ws parameter in the callback.
 
+
+
 setInterval(function() {
   console.log("Checking users");
   console.log(clients);
@@ -26,3 +29,8 @@ setInterval(function() {
   });
 }, 60000);
 // }, 30 * 60 * 1000); //every 30 minutes
+
+app.get('/' (req, res) => {
+  res.status(200).send(`<html><body>You must enter a valid email and a password to register. <a href="/register">Try again.</a></body></html>\n`);
+  res.render('/')
+});
