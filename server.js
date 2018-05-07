e865871abb33d69f9b39efb5b82a0616dd0df62c
Brian Holden <bkavuh@gmail.com>
2018-05-07 23:24:12 +0000

    more working server routes

--- server.js
+++ server.js
@@ -2,7 +2,8 @@
 var express = require("express");
 const settings = require("./settings");
 var app = express();
-const pg = require('pg')
+const pg = require('pg');
+
 var bcrypt = require('bcryptjs');
 var express = require('express')(),
     mailer = require('express-mailer');
@@ -10,7 +11,6 @@ var PORT = process.env.PORT || 3000; // default port 3000
 const bodyParser = require("body-parser");
 
 function clientbuilder (source){
-  console.log(source);
   const client = new pg.Client({
     user     : source.user,
     password : source.password,
@@ -21,6 +21,14 @@ function clientbuilder (source){
   });
   return client;
 }
+// heroku client connect
+// function clientbuilder (source){
+//   const client = new pg.Client({
+//     connectionString: process.env.DATABASE_URL,
+//     ssl: true,
+//   });
+//   return client;
+// }
 
 const client = clientbuilder(settings);
 client.connect((err) => {
@@ -70,49 +78,114 @@ mailer.extend(app, {
 
 
 function checkInCheck() {
+  var emails = [];
+  var contactList = '';
   for (var user in activeusers){
     activeusers[user].count += 1;
-    console.log("test up", user, activeusers[user].count);
-    if (activeusers[user].count > 10){
-      const emails = pullEmailList(user);
-      for (var i = 0; i < emails.length; i++){
-        sendEmail(emails[i]);
-      }
-    }
-  };
-}
 
-function pullEmailList(user){
-var results = [ ];
-console.log(user)
-  client.query("SELECT id FROM users WHERE email LIKE '%" + user + "%'", (err, result) => {
+    if (activeusers[user].count > 10){
+
+      client.query("SELECT id FROM users WHERE email LIKE '%" + user + "%'", (err, result) => {
     if (err) {
       return console.error("error running query", err);
     }
-    console.log(result.rows[0].id)
-    client.query("SELECT list FROM contacts WHERE id IN (" + result.rows[0].id + ")", (err, result) => {
+
+    client.query("SELECT contact_id FROM contacts WHERE owner_id = (" + result.rows[0].id + ")", (err, result) => {
       if (err) {
         return console.error("error running query", err);
       }
-      console.log(result.rows[0].list)
-      const emailList = result.rows[0].list[0];
-      for (var i = 1; i < result.rows[0].list.length; i++){
-        emailList += ', ' + result.rows[0].list[i];
+
+      contactList += result.rows[0].contact_id;
+      for (var i = 1; i < result.rows.length; i++){
+        contactList += ", " + (result.rows[i].contact_id);
       }
-      console.log(emailList);
-      client.query("SELECT email FROM users WHERE id IN (" + emailList + ")", (err, result) => {
+
+      client.query("SELECT email FROM users WHERE id IN (" + contactList + ")", (err, result) => {
         if (err) {
           return console.error("error running query", err);
         }
-        console.log(result.rows)
         for (var i = 0; i < result.rows.length; i++){
-          results.push(result.rows[i].email)
+          emails.push(result.rows[i].email);
+        }
+
+        console.log("emails",emails);
+      // for (var i = 0; i < emails.length; i++){
+      //  sendEmail(emails[i]);
+      // }
+        });
+      });
+     });
+   }
+  };
+}
+
+function addContact(user, email, name){
+var owner;
+var contact;
+  client.query("SELECT EXISTS (SELECT 1 FROM user WHERE email LIKE '%"+ email+"%'", (err, result) => {
+    if (err) {
+      return console.error("error running query", err);
+    }
+    if (result.rows[0] == 'f'){
+      client.query("INSERT INTO users (email) VALUES (" + email + ")", (err, result) => {
+        if (err) {
+          return console.error("error inserting query", err);
+        }
+      });
+    }
+  });
+
+  client.query("SELECT id FROM users WHERE email LIKE '%" + email + "%'", (err, result) => {
+    if (err) {
+      return console.error("error running query", err);
+    }
+    contact = result.rows[0].id;
+    client.query("SELECT id FROM users WHERE email LIKE '%" + user + "%'", (err, result) => {
+      if (err) {
+        return console.error("error running query", err);
+      }
+      owner = result.rows[0].id;
+      client.query("INSERT INTO contacts (owner_id, contact_id, nickname) VALUES (" + owner + ", " + contact + ", " + name + ")", (err, result) => {
+        if (err) {
+          return console.error("error inserting query", err);
         }
       });
     });
   });
-  return results;
 }
+
+// async function pullEmailList(user){
+// var contactList = '';
+// var results = [];
+// // console.log(user)
+//   client.query("SELECT id FROM users WHERE email LIKE '%" + user + "%'", (err, result) => {
+//     if (err) {
+//       return console.error("error running query", err);
+//     }
+//     // console.log(result.rows[0].id)
+//     client.query("SELECT contact_id FROM contacts WHERE owner_id = (" + result.rows[0].id + ")", (err, result) => {
+//       if (err) {
+//         return console.error("error running query", err);
+//       }
+//       // console.log("HERE",result.rows[0].contact_id)
+//       contactList += result.rows[0].contact_id;
+//       for (var i = 1; i < result.rows.length; i++){
+//         contactList += ", " + (result.rows[i].contact_id);
+//       }
+//       // console.log(contactList);
+//       client.query("SELECT email FROM users WHERE id IN (" + contactList + ")", (err, result) => {
+//         if (err) {
+//           return console.error("error running query", err);
+//         }
+//         for (var i = 0; i < result.rows.length; i++){
+//           results = [result.rows[i].email];
+//         }
+//         console.log("results", results);
+//         return results;
+//       });
+//     });
+//   });
+// }
 
 function sendEmail(email){
   app.mailer.send('email', {
@@ -144,6 +217,11 @@ app.get("/login/:id", (req, res) => {
 
 app.get("/logout/:id", (req, res) => {
   delete activeusers[req.params.id];
+  res.redirect("http://localhost:8080");
+});
+
+app.post("/update/:id", (req, res) => {
+  addContact(req.params.id, req.body.email, req.body.name);
   res.redirect("http://localhost:8080");
 });
 
